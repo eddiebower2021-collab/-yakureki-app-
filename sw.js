6fdec51cc1e7598499fea24d9a8bf1ab252e5cab
eddiebower2021-collab <eddiebower2021@gmail.com>
2026-07-10 09:26:00 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "yakureki-v0.2.0";
+const ASSETS = ["./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// アプリ本体はキャッシュ優先(オフライン起動)。それ以外はネットワークへ
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request, { ignoreSearch: true }).then(hit =>
+        hit || fetch(e.request).then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+          return res;
+        })
+      )
+    );
+  }
+});
